764b69fa2ff71d08e0b3072a48cdb49daa770c76
Alvaro <johndoe@example.com>
2024-10-15 20:12:32 +0100

    update

--- notes/urls.py
+++ notes/urls.py
@@ -6,4 +6,5 @@ app_name = 'notes'
 urlpatterns = [
     path('', views.home, name='home'),
     path('note/<note_slug>/', views.note_detail, name='note_detail'),
+    path('add_note', views.add_note, name='add_note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,10 @@
 
-from django.shortcuts import render
+
+from django.shortcuts import redirect, render
+from django.utils.text import slugify
 from notes.models import Note
 
+from .forms import AddNoteForm
 
 def home(request):
     num_notes = Note.objects.count()
@@ -16,3 +19,28 @@ def home(request):
 def note_detail(request, note_slug: str):
     note = Note.objects.get(slug=note_slug)
     return render(request, 'notes/detail.html', dict(note=note))
+
+
+
+def add_note(request):
+    if request.method == 'POST':
+        if (form := AddNoteForm(request.POST)).is_valid():
+
+
+            post = form.save(commit=False)
+
+
+            post.slug = slugify(post.title)
+
+
+            post.save()
+
+
+            return redirect('notes:home')
+
+
+    else:
+        form = AddNoteForm()
+
+
+    return render(request, 'notes/add_note.html', dict(form=form))
